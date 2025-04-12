53ffcc810dfe4b50e47f6129a18b8bb7632bfb4e
Abhay-singh-rathore <abhaysinghrathore756@gmail.com>
2025-04-12 16:56:34 +0530

    button postion change update

--- app/src/main/java/com/example/calci_x/MainActivity.kt
+++ app/src/main/java/com/example/calci_x/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.calci_x
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,39 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+            val reultTV = findViewById<TextView>(R.id.result_tv)
+            val solutionTV = findViewById<TextView>(R.id.solution_tv)
+
+            val buttonc = findViewById<MaterialButton>(R.id.button_clear)
+            val buttonac = findViewById<MaterialButton>(R.id.button_allclear)
+
+            val buttonrightbracket = findViewById<MaterialButton>(R.id.button_rightbracket)
+            val buttonleftbracket = findViewById<MaterialButton>(R.id.button_leftbracket)
+            val buttondivide = findViewById<MaterialButton>(R.id.button_Divide)
+            val buttonmultiply = findViewById<MaterialButton>(R.id.button_multiply)
+            val buttonsubstract = findViewById<MaterialButton>(R.id.button_substract)
+            val buttonadd = findViewById<MaterialButton>(R.id.button_Add)
+            val buttonequal = findViewById<MaterialButton>(R.id.button_equal)
+            val buttonzero = findViewById<MaterialButton>(R.id.button_zero)
+            val buttonone = findViewById<MaterialButton>(R.id.button_one)
+            val buttontwo = findViewById<MaterialButton>(R.id.button_Two)
+            val buttonthree = findViewById<MaterialButton>(R.id.button_three)
+            val buttonfour = findViewById<MaterialButton>(R.id.button_four)
+            val buttonfive = findViewById<MaterialButton>(R.id.button_five)
+            val buttonsix = findViewById<MaterialButton>(R.id.button_six)
+            val buttonseven = findViewById<MaterialButton>(R.id.button_seven)
+            val buttoneight = findViewById<MaterialButton>(R.id.button_eight)
+            val buttonnine = findViewById<MaterialButton>(R.id.button_nine)
+            val buttonpoint = findViewById<MaterialButton>(R.id.button_dot)
+
+
+        }
+
+         fun onButtonClick(view: View){
+            val button=view as MaterialButton
+            val buttonText=button.text.toString()
+             val solutionTV = findViewById<TextView>(R.id.solution_tv)
+             solutionTV.append(buttonText)
         }
     }
 }
